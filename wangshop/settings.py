75f5b331859f260aefa1fdd57c2f02f77bfd9b52
wangzw <wzw571603974@qq.com>
2020-03-20 17:28:24 +0800

    MySQL连接

--- wangshop/settings.py
+++ wangshop/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'wangshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop',
+        'USER': 'root',
+        'PASSWORD': '19981008',
+        'HOST': '47.106.77.191',
+        'PORT': '3306',
     }
 }
 
