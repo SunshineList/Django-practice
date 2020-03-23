bae9424642bcb453257877f15b5252b467c17f6f
wangzw <wzw571603974@qq.com>
2020-03-23 09:36:20 +0800

    update restframework url show

--- account/rest/urls.py
+++ account/rest/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from account.rest import api
+from account.rest.router import HybridRouter
 
-router = DefaultRouter()
+# router = DefaultRouter()
+
+# urlpatterns = [
+#     path('login/', api.login, name='login'),
+#     path('logout/', api.logout, name='logout')
+# ]
+
+functions_and_view_classes = (
+    ('登录', path('login/', api.login, name='restv1-login')),
+    ('退出登录', path('logout', api.logout, name='restv1-logout'))
+)
+
+router = HybridRouter()
+for api_name, func in functions_and_view_classes:
+    router.add_api_view(api_name, func)
 
 urlpatterns = [
-    path('login/', api.login, name='login'),
-    path('logout/', api.logout, name='logout')
+    path('', include(router.urls))
 ]
