14c57a2210c0af2d294b3dac59661a2b81e572a4
wangzw <wzw571603974@qq.com>
2020-03-25 23:16:24 +0800

    add ip for admin

--- account/admin.py
+++ account/admin.py
@@ -12,7 +12,7 @@ class UserAdmin(UserAdmin):
     list_display = ('type', 'username', 'email', 'is_staff')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'type', 'birthday',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'type', 'birthday', "ips")}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
